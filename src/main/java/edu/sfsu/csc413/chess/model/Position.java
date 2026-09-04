047d5293d2972be40e8d365bafbf0e0f7622c4bb
Gevilee <157779885+gverango@users.noreply.github.com>
2026-09-04 13:27:25 -0700

    m0b: set up Position.java, tests still fail

--- src/main/java/edu/sfsu/csc413/chess/model/Position.java
+++ src/main/java/edu/sfsu/csc413/chess/model/Position.java
@@ -2,6 +2,25 @@ package edu.sfsu.csc413.chess.model;
 
 //record header
 public record Position(int file, int rank) {
+    // files and rank run 0-7
+    public static final int BOARD_SIZE = 8;
+    // true if raw coordinate is a real square
+    public static boolean isOnBoard(int file, int rank) {
+        return file >= 0 && file < BOARD_SIZE && rank >= 0 && rank < BOARD_SIZE;
+        }
+    }
+    //compact constructor
+    public Position {
+        if (!isOnBoard(file,rank)) {
+            throw new IllegalArgumentException(
+                    "Position off board: file=" + file + ",rank=" + rank
+            );
+        }
+    }
+    @Override
+    public String toString() {
+        return "" + (char)('a'+file) + (char)('1'+rank); // int added to char ascii val to represent file
+    }
 
     public static Position parse(String algebraic) {
         return null;
