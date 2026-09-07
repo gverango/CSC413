db7e338edbc13ab004cd517c12509ddfe00a833f
Gevilee <157779885+gverango@users.noreply.github.com>
2026-09-07 08:33:55 -0700

    m0b: implemented switch statements for Color.java

--- src/main/java/edu/sfsu/csc413/chess/model/Color.java
+++ src/main/java/edu/sfsu/csc413/chess/model/Color.java
@@ -16,24 +16,35 @@ public enum Color {
 
     /** The side whose turn it is after this one moves. */
     public Color opposite() {
-        throw new UnsupportedOperationException("M0b: your turn");
+        return switch (this){
+            case WHITE -> BLACK;
+            case BLACK -> WHITE;
+        };
     }
-
     /**
      * The direction pawns of this color advance, measured in ranks.
      * White moves up the board (+1), black moves down (-1).
      */
     public int pawnDirection() {
-        throw new UnsupportedOperationException("M0b: your turn");
+        return switch (this){
+            case WHITE -> 1;
+            case BLACK -> -1;
+        };
     }
 
     /** The rank pawns of this color start on (0-based). */
     public int pawnStartRank() {
-        throw new UnsupportedOperationException("M0b: your turn");
+        return switch (this){
+            case WHITE -> 1;
+            case BLACK -> 6;
+        };
     }
 
     /** The rank a pawn of this color must reach to promote (0-based). */
     public int promotionRank() {
-        throw new UnsupportedOperationException("M0b: your turn");
+        return switch (this){
+            case WHITE -> 7;
+            case BLACK -> 0;
+        };
     }
 }
